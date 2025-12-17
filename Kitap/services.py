322a66765505ee70e89e4bf825ef39a0c308169d
Mustafa SAYDAN <mustafasaydan16@gmail.com>
2025-12-17 23:24:10 +0300

    Add service function to update a book

--- Kitap/services.py
+++ Kitap/services.py
@@ -23,3 +23,21 @@ def kitap_getir(kitap_id,kullanici_id):
     kitap=Kitap.query.filter_by(id=kitap_id,kullanici_id=kullanici_id).first()
 
     return kitap
+
+
+def kitap_guncelle(kitap_id,isim,yazar,sayfa_sayisi,kategori,yayin_yili,yayinevi,okundu_mu,kullanici_id):
+    kitap = Kitap.query.filter_by(id=kitap_id,kullanici_id=kullanici_id).first()
+    if not kitap:
+        return None
+
+    kitap.isim=isim
+    kitap.yazar=yazar
+    kitap.sayfa_sayisi=sayfa_sayisi
+    kitap.kategori=kategori
+    kitap.yayin_yili=yayin_yili
+    kitap.yayinevi=yayinevi
+    kitap.okundu_mu=okundu_mu
+
+    db.session.commit()
+
+    return kitap
